d5af368af0b61d3145fb7932df8619ea1e9043b7
Gianluca Bastos <gianlucaborgato@gmail.com>
2020-05-22 20:22:09 -0300

    Inserção de usuário com POST

--- src/main/java/com/gianluca/workshopmongo/services/UserService.java
+++ src/main/java/com/gianluca/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gianluca.workshopmongo.domain.User;
+import com.gianluca.workshopmongo.dto.UserDTO;
 import com.gianluca.workshopmongo.repository.UserRepository;
 import com.gianluca.workshopmongo.services.excepetion.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
