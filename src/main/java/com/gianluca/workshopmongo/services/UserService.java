7e142b665b2e767cba3a95cc53546e4b32fad78c
Gianluca Bastos <gianlucaborgato@gmail.com>
2020-05-22 20:08:35 -0300

    Obtendo usuário por id

--- src/main/java/com/gianluca/workshopmongo/services/UserService.java
+++ src/main/java/com/gianluca/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.gianluca.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gianluca.workshopmongo.domain.User;
 import com.gianluca.workshopmongo.repository.UserRepository;
+import com.gianluca.workshopmongo.services.excepetion.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> fildAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
