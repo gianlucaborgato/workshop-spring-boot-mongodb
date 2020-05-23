4c5847e959cddd45ea9cbd76664730612a852c5c
Gianluca Bastos <gianlucaborgato@gmail.com>
2020-05-22 21:32:03 -0300

    Obtendo um post por id

---
+++ src/main/java/com/gianluca/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.gianluca.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gianluca.workshopmongo.domain.Post;
+import com.gianluca.workshopmongo.repository.PostRepository;
+import com.gianluca.workshopmongo.services.excepetion.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
