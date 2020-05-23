985134e84623c7b23a3c6ccdd424137d45b98e1b
Gianluca Bastos <gianlucaborgato@gmail.com>
2020-05-22 21:40:49 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/gianluca/workshopmongo/domain/Post.java
+++ src/main/java/com/gianluca/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.gianluca.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.gianluca.workshopmongo.dto.AuthorDTO;
+import com.gianluca.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,7 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -77,6 +81,14 @@ public class Post implements Serializable {
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
